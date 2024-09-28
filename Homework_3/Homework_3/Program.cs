fed802a576148aa1d4f6f6304b613acb8ad7e3c7
MauriS02 <llexxa2002@gmail.com>
2024-09-28 19:06:21 +0300

    Commit_1

---
+++ Homework_3/Homework_3/Program.cs
@@ -0,0 +1,30 @@
+﻿
+var vvod = (Console.ReadLine());
+int znachenie = int.Parse(vvod);
+int[] massiv = {znachenie};
+
+if (vvod != "q")
+{
+    for (int i = 0;; i++)
+    {
+        Console.WriteLine($"Значение массива i: {massiv[i]}");
+        
+        vvod = Console.ReadLine();
+        znachenie = int.Parse(vvod);
+        massiv[i] = znachenie;
+
+        Console.WriteLine($"Длина массива:{massiv.Length}");
+    }
+}
+else
+{
+    
+}
+
+
+
+
+/* Вводим значение в массив. Если значение Q заканчиваем программу
+ * Если значение - число - увеличиваем массив и записываем туда значение
+ * 
+*/
