6c501287c16e4571a8e15a41afbae04cde54d87c
MauriS02 <llexxa2002@gmail.com>
2024-10-06 13:26:47 +0300

    commit

---
+++ Homework_5_1/Homework_5_1/Program.cs
@@ -0,0 +1,51 @@
+﻿Console.WriteLine("Введите номер простого числа");
+
+
+
+var vvod = Console.ReadLine();
+var parse = int.TryParse(vvod, out int q);
+int count = 1;
+var number = 0;
+
+if (parse == true)
+{
+    for (int i = 1; i <= q; i++)
+    {
+        number = count / 1;
+
+        if (number == count)
+        {
+            number = count / count;
+            if (number == 1)
+            {
+                for (int y = 1; y == i; y++)
+                {
+                    number = count % y;
+                    if (number == 0)
+                    {
+                        count++;
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Введено не простое число");
+                        break;
+                    }
+                    count++;
+                }
+            }
+        }
+        
+    }
+    Console.WriteLine($"Порядковый номер вашего простого числа: {count - 1}");
+}
+else 
+{
+    Console.WriteLine("Введено не число");
+}
+
+
+
+
+
+
