ced753aceaf523c164767d16b4e512ae11e456f9
MauriS02 <llexxa2002@gmail.com>
2024-10-06 17:05:54 +0300

    commit

--- Homework_5_1/Homework_5_1/Program.cs
+++ Homework_5_1/Homework_5_1/Program.cs
@@ -1,51 +1,57 @@
-﻿Console.WriteLine("Введите номер простого числа");
+﻿using System.Threading.Tasks.Sources;
 
-
+/*Console.WriteLine("Введите номер простого числа");
 
 var vvod = Console.ReadLine();
 var parse = int.TryParse(vvod, out int q);
 int count = 1;
-var number = 0;
+int score = 0;
+int final = 1;
 
 if (parse == true)
 {
     for (int i = 1; i <= q; i++)
     {
-        number = count / 1;
-
-        if (number == count)
+        if (score == 2)
         {
-            number = count / count;
-            if (number == 1)
-            {
-                for (int y = 1; y == i; y++)
-                {
-                    number = count % y;
-                    if (number == 0)
-                    {
-                        count++;
-                        continue;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Введено не простое число");
-                        break;
-                    }
-                    count++;
-                }
-            }
+            final++;
         }
-        
+        count = 1;
+
+        for (int y = 1; y <= i; y++)
+        {
+            if (i % y == 0)
+            {
+                count++;
+                score = count - 1;
+                continue;
+            }
+
+        }
+
     }
-    Console.WriteLine($"Порядковый номер вашего простого числа: {count - 1}");
+    Console.WriteLine($"Порядковый номер вашего простого числа: {final}");
 }
-else 
+
+else
 {
     Console.WriteLine("Введено не число");
+}*/
+
+Console.WriteLine("Введите номер простого числа");
+
+var vvod = Console.ReadLine();
+var parse = int.TryParse(vvod, out int q);
+int count = 1;
+int score = 0;
+int final = 1;
+
+if (parse == true)
+{
+    for (int i = 0; i <= 4; i++)
+    {
+
+    }
 }
-
-
-
-
 
 
