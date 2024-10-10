bec45e23a58e3ebcd305bdee9d36b09806f57f2e
Alexey <kozlovskiy_av@ascon.ru>
2024-10-10 17:06:28 +0300

    Update Program.cs

--- Homework_Chicken/Homework_Chicken/Program.cs
+++ Homework_Chicken/Homework_Chicken/Program.cs
@@ -8,8 +8,80 @@
 При условии если курица накормлена, то яйцо высиживается в этот ход и только одно*/
 
 using System.Security.Cryptography;
+using System;
 
-enum Chicken
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("У вас три голодные курицы.");
+        Console.WriteLine("Выберите номер действия, которое хотите выполнить:");
+        Console.WriteLine("1. Покормить курицу");
+        Console.WriteLine("2. Забрать яйцо");
+        Console.WriteLine("3. Ничего не делать");
+
+        var vvod = Console.ReadLine();
+        var parse = int.TryParse(vvod, out int q);
+
+        switch (q)
+        {
+            case 1:
+                Console.WriteLine("");
+                Food(Chicken.WhiteChicken);
+                break;
+            case 2:
+                Console.WriteLine("");
+                Food(Chicken.BlackChicken);
+                break;
+            case 3:
+                Console.WriteLine("");
+                Food(Chicken.YellowChicken);
+                break;
+
+        }
+
+
+        void Food(Chicken cipa)
+        {
+            Console.WriteLine("Выберите курицу которую хотите покормить:");
+            Console.WriteLine($"1.{cipa}");
+            Console.WriteLine("Кормим курицу");
+
+        }
+    }
+
+
+    /* void CollectEgg()
+     {
+
+     }
+
+     void DoNothing()
+     {
+     }*/
+
+    enum Chicken
+    {
+        WhiteChicken = 0,
+        BlackChicken = 1,
+        YellowChicken = 2
+    }
+
+    enum Action
+    {
+    Feed = 1,
+    TakeEgg = 2,
+    Nothing = 3
+    }
+
+    int hungry = 0;
+    int satiety = 0;
+    int eggs = 0;
+
+ 
+}
+
+/*enum Chicken
 {
     WhiteChicken = 0,
     BlackChicken = 1,
@@ -68,5 +140,5 @@ void CollectEgg()
 void DoNothing()
 {
 
-}
+}*/
 
